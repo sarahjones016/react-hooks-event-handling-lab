78739c2e501e0288d574d1ec08b9306d0aeba297
Sarah Jones <sarahrjones8@gmail.com>
2023-04-10 14:05:27 -0400

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
 
-function Keypad (){
+import React from 'react'
+
+function Keypad () {
+   function handleChange() {
+        console.log("Entering password...")
+   }
     return (
-        <div></div>
+        <div>
+            <input 
+                type="password" 
+                onChange={handleChange}
+            />
+        </div>
     )
 }
 
